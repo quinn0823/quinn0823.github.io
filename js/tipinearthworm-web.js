baaed45158f0f91017752e9a12e3a22c6de4aa62
Quinn0823 <3547347506@qq.com>
2021-07-06 14:03:39 +0800

    🕳 Repair TIPS‘ Bug in Earthworm-Web

--- js/tipinearthworm-web.js
+++
@@ -1,7 +0,0 @@
-setTimeout(tip(), 1000);
-function tip(){
-	var tip=confirm("⚠ Warning:\nEarthworm-Web is no longer updated.\n\n🔗 compare:\nQuinn-Web is more beautiful than Earthworm-Web.\nEarthworm-Web is richer than Quinn-Web.\n\nClick 'OK' to go to Quinn-Web, and click 'Cancel' to stay here.");
-	if (tip == true){
-		window.location=("https://quinn0823.github.io");
-	}
-}
