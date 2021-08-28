ea6dee6d6dd9ce1dc0fa549afd779e979007ffe2
Quinn0823 <3547347506@qq.com>
2021-08-28 13:51:01 +0800

    💻 Create Web.js

---
+++ js/web.js
@@ -0,0 +1,4 @@
+setTimeout(tip(), 0);
+function tip(){
+    alert("Quinn-web will begin closure construction on August 27, 2021 and is expected to resume operations during China's National Day (October 1-7, 2021). In the process of closed construction, Quinn-web may have some phenomena such as 404, text disorder and so on. Please forgive!")；
+}
