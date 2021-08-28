33456b779d62490ac519a07ffcf091e8a83a385d
Qiu Qixuan <3547347506@qq.com>
2021-08-28 15:03:12 +0800

    🔼 Update tip in home page

--- js/web.js
+++ js/web.js
@@ -1,7 +1,7 @@
 setTimeout(tip(), 0);
 function tip(){
-    var tip = confirm("⚠ Quinn-Web began closure construction on August 27, 2021 and is expected to resume operations during National Day of the People's Republic of China (October 1-7, 2021). \n\n🌐 In the process of closed construction, Quinn-web may have some phenomena such as 404, text disorder and so on. \n\n🤝🏻 Please forgive!\n\n👉🏻 Click 'Cancel' to switch to Chinese.");
+    var tip = confirm("⚠ Quinn-Web began closure construction on August 27, 2021 and is expected to resume operations during National Day of the People's Republic of China (October 1-7, 2021). \n\n🌐 In the process of closed construction, Quinn-Web may have some phenomena such as 404, text disorder and so on. \n\n🤝🏻 Please forgive!\n\n👉🏻 Click 'Cancel' to switch to Chinese.");
     if(tip == false) {
-        alert("⚠ Quinn-Web 于2021年8月27日开始封闭施工，预计在中华人民共和国国庆节（2021年10月1日至7日）期间恢复运营。\n\n🌐 在封闭施工过程中，Quinn-web 可能会出现404、样式错乱等现象。\n\n🤝🏻 请谅解！");
+        alert("⚠ Quinn-Web 于2021年8月27日开始封闭施工，预计在中华人民共和国国庆节（2021年10月1日至7日）期间恢复运营。\n\n🌐 在封闭施工过程中，Quinn-Web 可能会出现404、样式错乱等现象。\n\n🤝🏻 请谅解！");
     }
 }
