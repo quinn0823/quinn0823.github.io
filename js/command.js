8c4b9b4f7fe0f8e58a4138698a79a9e5276f09dc
Qiu Qixuan <3547347506@qq.com>
2021-12-04 00:00:53 +0800

    🆙 Command Test
    Former-commit-id: 1734c031a7fb34976b4f5076433dba1436091698

--- js/command.js
+++ js/command.js
@@ -1,12 +1,5 @@
 function command() {
     var command = document.getElementById('command').value;
-    if (command == "Home" || command == "home") {
-        self.location = '/';
-    } else if (command == "About" || command == "about") {
-        self.location = '/about';
-    } else if (command == "Test" || command == "test") {
-        self.location = '/test';
-    } else {
-        alert("Error");
-    }
+    self.location = 'https://quinn0823.github.io/'+command;
+    
 }
