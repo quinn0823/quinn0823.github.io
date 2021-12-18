d7f112db6155bdc79015f805d812dfb863e0accc
Qiu Qixuan <3547347506@qq.com>
2021-12-18 16:31:35 +0800

    ✍🏻 edit js code
    Former-commit-id: b518a500c28c2afb872c68e7853d74020a4087f7

--- js/click.js
+++ js/click.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        self.location = 'https://quinn0823.github.io/home';
+        window.location = 'https://quinn0823.github.io/home';
     });
 });
