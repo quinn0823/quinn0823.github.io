0abcf26b12ee4932828debef1fba068087101ac4
Quinn0823 <3547347506@qq.com>
2022-02-04 13:07:23 +0800

    🏳️‍🌈 change wizard
    Former-commit-id: c5d95089e2cfe5db656aa5a98e5c656910a9d499

--- h_versions/v10.0/js/click.js
+++ h_versions/v10.0/js/click.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        window.location = 'https://quinn0823.github.io/home';
+        window.location = 'https://quinn0823.github.io/h_versions/home';
     });
 });
