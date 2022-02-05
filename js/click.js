ba97910648008c2b5e27458e34e728fb9299f67f
Quinn0823 <3547347506@qq.com>
2022-02-05 11:13:33 +0800

    🏳️‍🌈 macOS style menu
    Former-commit-id: 9680cc2875840defdfa89600cd28815a542d6efb

--- js/click.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
-    $("body").click(function(e) {
-        window.location = 'https://quinn0823.github.io/home';
-    });
-});
--- js/command.js
+++
@@ -1,4 +0,0 @@
-function command() {
-    var command = document.getElementById('command').value;
-    window.location = 'https://quinn0823.github.io/'+command;
-}
