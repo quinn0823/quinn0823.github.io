11e988482c6ec9e0669d63dc9d7878fce5fba6fc
Quinn0823 <3547347506@qq.com>
2022-02-08 13:16:24 +0800

    🧑‍💻 program optimized
    Former-commit-id: 949b898d3942b8b3e7a00d7aa80e5902b981cd33

--- js/system.js
+++ js/system.js
@@ -8,7 +8,10 @@ var bIsAndroid = sUserAgent.match(/android/i) == "android";
 var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
 var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
 if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
-    self.location = '/m';
+    var tip = confirm("You are using phone/pad to visit Quinn-Web. Do you want to go to the phone/pad version?");
+    if (tip == ture) {
+        self.location = '/m';
+    }
 } else {
     
 }
