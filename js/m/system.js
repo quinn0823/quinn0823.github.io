d07e9a5fb69ba60a94870ebed5345ba6cd76aaba
Quinn0823 <3547347506@qq.com>
2022-02-08 11:56:31 +0800

    📱 new pages for mobile
    Former-commit-id: e2f48ce37c39f4c1723228e36c5154cfb84f5eb5

---
+++ js/m/system.js
@@ -0,0 +1,16 @@
+function judgmentClient(PCurl,WebUel){
+    var UserClient = navigator.userAgent.toLowerCase();
+    var IsIPad = UserClient.match(/ipad/i) == "ipad";
+    var IsIphoneOs = UserClient.match(/iphone os/i) == "iphone os";
+    var IsMidp = UserClient.match(/midp/i) == "midp";
+    var IsUc7 = UserClient.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var IsUc = UserClient.match(/ucweb/i) == "ucweb";
+    var IsAndroid = UserClient.match(/android/i) == "android";
+    var IsCE = UserClient.match(/windows ce/i) == "windows ce";
+    var IsWM = UserClient.match(/windows mobile/i) == "windows mobile";
+    if (IsIPad || IsIphoneOs || IsMidp || IsUc7 || IsUc || IsAndroid || IsCE || IsWM){
+        
+    } else {
+        window.location = '/';
+    }
+}
