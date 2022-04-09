abe40a70642d13bfcae02468a008e4cc3393b627
Quinn0823 <3547347506@qq.com>
2022-04-09 20:08:44 +0800

    🔗 Adjust URL of Web History Page
    Former-commit-id: 1929fff4ef0b5e2448789bf8b249c385fb457895
    Former-commit-id: 677898aa555134f6ec2d971a67f944e0c7152546

--- h_versions/v10.0/js/click.js
+++ h_versions/v10.0/js/click.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        window.location = 'https://quinn0823.github.io/h_versions/v10.0/home';
+        window.location = 'https://quinn0823.github.io/web_history/home';
     });
 });
