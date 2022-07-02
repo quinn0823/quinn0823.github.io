67b00975b9648c80fbfbdd4228d725c6dded0691
Quinn0823 <3547347506@qq.com>
2022-07-02 18:04:20 +0800

    ⌨ simplified code
    Former-commit-id: 64c8205abb88ee7fdee5f4647f6e05fc7a1697d9
    Former-commit-id: 87ebc6cacf58e52b7bfca73edb9b45abad92cb2a
    Former-commit-id: 1ecb23a6d1096e42d2f726b346e35d772e0dc6b1

--- js/page.js
+++ js/page.js
@@ -11,13 +11,13 @@ langchange();
 function langchange() {
     if(document.getElementById("lang").value=="1") {
         document.title = title_en + " | Quinn-Web";
-        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a>Media</a><ul><li><a href='/music.html'>Music</a></li><li><a href='/photos.html'>Photos</a></li><li><a href='/videos.html'>Videos</a></li></ul></li><li class='about'><a href='/about'>About</a></li></ul></div><h1>" + title_en + "</h1>";
+        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a>Media</a><ul><li><a href='/music.html'>Music</a></li><li><a href='/photos.html'>Photos</a></li><li><a href='/videos.html'>Videos</a></li></ul></li><li class='about'><a href='/about'>About</a></li></ul></div><h1>" + title_en + "</h1><hr />";
         document.getElementById("en").style.display = "block";
         document.getElementById("cns").style.display = "none";
         document.getElementById("footer").innerHTML="<span style='text-align: center; display:block; '>📅 Last Update: July 2, 2022 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn0823 All Rights Reserved.</span>";
     } else if(document.getElementById("lang").value == "2") {
         document.title = title_cns + " | Quinn-Web";
-        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>主页</a></li><li><a href='/projects.html'>项目</a></li><li><a>媒体</a><ul><li><a href='/music.html'>音乐</a></li><li><a href='/photos.html'>照片</a></li><li><a href='/videos.html'>视频</a></li></ul></li><li class='about'><a href='/about'>About</a></li></ul></div><h1>" + title_cns + "</h1>";
+        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>主页</a></li><li><a href='/projects.html'>项目</a></li><li><a>媒体</a><ul><li><a href='/music.html'>音乐</a></li><li><a href='/photos.html'>照片</a></li><li><a href='/videos.html'>视频</a></li></ul></li><li class='about'><a href='/about'>About</a></li></ul></div><h1>" + title_cns + "</h1><hr />";
         document.getElementById("cns").style.display = "block";
         document.getElementById("en").style.display = "none";
         document.getElementById("footer").innerHTML="<span style='text-align: center; display:block; '>📅 最近更新：2022年7月2日 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn0823 保留所有权利。</span>";
