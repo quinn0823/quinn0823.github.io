859acaffcc4c96fc56ee5ebf2989e732f254795f
Quinn0823 <3547347506@qq.com>
2022-07-02 21:24:32 +0800

    🦶 new footer
    Former-commit-id: da8eedd01036193b009ca6b1d2808ecd62b6a588
    Former-commit-id: 5b51acd871d922473397ad209b076e65c30f6963
    Former-commit-id: 421c0f062e59727726cdd3d2af01a13d3248c6be
    Former-commit-id: 44c19285b4f13a3d09a7d75cc5b6a7c5a6de4b9b
    Former-commit-id: c8a2259aeafc9efbe954db686a4e41d039405078
    Former-commit-id: 2758a4792da54fbd14424da4db84cda8f00be459
    Former-commit-id: 30078735ade4c4bdc47aecb253d67f2aabf450a6
    Former-commit-id: b0f55cd44a5a81312c23bc7022b1fbccbb15cc43
    Former-commit-id: eb465050a53df45f0af55fbd6ef5309dbf365613
    Former-commit-id: e37534b0a93279306d975fac0fe233c082d84e0b
    Former-commit-id: 3f707b66a227dca2cb1fe1ec75bd45da2e230068
    Former-commit-id: 93d7b7557db692324c324966cd4b5b06796c1f2a
    Former-commit-id: 0607979315b099b4d755f919910639d02122f77e

--- js/page.js
+++ js/page.js
@@ -11,15 +11,15 @@ langchange();
 function langchange() {
     if(document.getElementById("lang").value=="1") {
         document.title = title_en + " | Quinn-Web";
-        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a>Media</a><ul><li><a href='/music.html'>Music</a></li><li><a href='/photos.html'>Photos</a></li><li><a href='/videos.html'>Videos</a></li></ul></li><li class='about'><a href='/about'>About</a></li></ul></div><h1>" + title_en + "</h1><hr />";
+        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a href='/media.html'>Media</a><ul><li><a href='/media/music.html'>Music</a></li><li><a href='/media/photos.html'>Photos</a></li><li><a href='/media/videos.html'>Videos</a></li></ul></li><li class='about'><a href='/about.html'>About</a></li></ul></div><h1>" + title_en + "</h1><hr />";
         document.getElementById("en").style.display = "block";
         document.getElementById("cns").style.display = "none";
-        document.getElementById("footer").innerHTML="<span style='text-align: center; display:block; '>📅 Last Update: July 2, 2022 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn0823 All Rights Reserved.</span>";
+        document.getElementById("footer").innerHTML="<span style='text-align: center; display:block; '>Because of technical reasons, Quinn-Web 12 can't adapt to phones as well as <a href='/web_history/v10.0' target='_blank'>Quinn-Web v10.0</a>. We are working on solving this problem. <hr />📅 Last Update: July 2, 2022 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn0823 All Rights Reserved.</span>";
     } else if(document.getElementById("lang").value == "2") {
         document.title = title_cns + " | Quinn-Web";
-        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>主页</a></li><li><a href='/projects.html'>项目</a></li><li><a>媒体</a><ul><li><a href='/music.html'>音乐</a></li><li><a href='/photos.html'>照片</a></li><li><a href='/videos.html'>视频</a></li></ul></li><li class='about'><a href='/about'>About</a></li></ul></div><h1>" + title_cns + "</h1><hr />";
+        document.getElementById("header").innerHTML = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>主页</a></li><li><a href='/projects.html'>项目</a></li><li><a href='/media.html'>媒体</a><ul><li><a href='/media/music.html'>音乐</a></li><li><a href='/media/photos.html'>照片</a></li><li><a href='/media/videos.html'>视频</a></li></ul></li><li class='about'><a href='/about.html'>About</a></li></ul></div><h1>" + title_cns + "</h1><hr />";
         document.getElementById("cns").style.display = "block";
         document.getElementById("en").style.display = "none";
-        document.getElementById("footer").innerHTML="<span style='text-align: center; display:block; '>📅 最近更新：2022年7月2日 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn0823 保留所有权利。</span>";
+        document.getElementById("footer").innerHTML="<span style='text-align: center; display:block; '>由于技术原因，Quinn-Web 12 不能像 <a href='/web_history/v10.0' target='_blank'>Quinn-Web v10.0</a> 一样很好地适应手机。我们正着力于解决这个问题。<hr />📅 最近更新：2022年7月2日 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn0823 保留所有权利。</span>";
     }
 }
