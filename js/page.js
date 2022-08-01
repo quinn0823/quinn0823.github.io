5894d817cbc5fdd6bb7f86e6fc1b14288ffaeb8c
Quinn0823 <3547347506@qq.com>
2022-08-01 21:28:29 +0800

    ▶ New Code!
    Former-commit-id: 68850253296c582bd20e45511aa68ab04670f461
    Former-commit-id: 0e3907ee991d47a077b6d33cbec6922f23bfd80e
    Former-commit-id: 847ce8cd4af0d23fbd9b5e0f1353263bb64cba6d
    Former-commit-id: dc6c2f93bfba49d26d246d5bc0c37fd7b4946167
    Former-commit-id: d756ad3dd4e07f42c20c64d80ea02da12c13e52a
    Former-commit-id: 7b218e7da4f10028fa24aa0c79576a61c99a340b
    Former-commit-id: e62aaf6cbce8265488b770ba1adbdff5681c28a4
    Former-commit-id: effb307c58146d107f9d91c7d14697b63174d9f6
    Former-commit-id: 39e34ce3b3861b1e488c6f73f687b69a6b89111d
    Former-commit-id: a03e142875af7032c8315a20aabacfc6fac8ab47
    Former-commit-id: e6481f21da7d5418242115839c8a88601acd57e5
    Former-commit-id: 848b756b2f9ae2f04e0cb391da2e486d73aac4df
    Former-commit-id: 57c5bb121e7f778da25bd05912c84630bc865cfe

---
+++ js/page.js
@@ -0,0 +1,57 @@
+var path = window.location.pathname, title_en = document.title, title_cns = document.title;
+if(path == "/index.html" || path == "/") {
+    title_cns = "首页";
+} else if(path == "/projects.html") {
+    title_cns = "项目";
+} else if(path == "/media.html") {
+    title_cns = "媒体";
+} else if(path == "/media/music.html") {
+    title_cns = "音乐";
+} else if(path == "/media/photos.html") {
+    title_cns = "照片";
+} else if(path == "/media/videos.html") {
+    title_cns = "视频";
+} else if(path == "/laboratory.html") {
+    title_cns = "实验室";
+} else if(path == "/about.html") {
+    title_cns = "关于";
+}
+
+var header_en = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a href='/media.html'>Media</a><ul><li><a href='/media/music.html'>Music</a></li><li><a href='/media/photos.html'>Photos</a></li><li><a href='/media/videos.html'>Videos</a></li></ul></li><li><a href='/laboratory.html'>Laboratory</a></li><li class='about'><a href='/about.html'>About</a></li></ul></div><h1>" + title_en + "</h1><hr />";
+var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>首页</a></li><li><a href='/projects.html'>项目</a></li><li><a href='/media.html'>媒体</a><ul><li><a href='/media/music.html'>音乐</a></li><li><a href='/media/photos.html'>照片</a></li><li><a href='/media/videos.html'>视频</a></li></ul></li><li><a href='/laboratory.html'>实验室</a></li><li class='about'><a href='/about.html'>关于</a></li></ul></div><h1>" + title_cns + "</h1><hr />";
+
+var footer_en = "<div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 Last Update: July 23, 2022 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn All Rights Reserved.</span>";
+var footer_cns = "<div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 最近更新：2022年7月23日 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " 邱琦轩 保留所有权利。</span>";
+
+langdate();
+
+function langdate() {
+    if(localStorage.langdate == 1) {
+        document.title = title_en + " | Quinn-Web";
+
+        document.getElementById("header").innerHTML = header_en;
+        document.getElementById("footer").innerHTML = footer_en;
+
+        document.getElementById("en").style.display="inline";
+        document.getElementById("cns").style.display="none";
+    } else if(localStorage.langdate == 2) {
+        document.title = title_cns + " | Quinn-Web";
+
+        document.getElementById("header").innerHTML = header_cns;
+        document.getElementById("footer").innerHTML = footer_cns;
+
+        document.getElementById("en").style.display="none";
+        document.getElementById("cns").style.display="inline";
+    } else {
+        localStorage.langdate = 1;
+    }
+} 
+
+function langchange() {
+    if(document.getElementById("lang").value=="1") {
+        localStorage.langdate = 1;
+    } else if(document.getElementById("lang").value == "2") {
+        localStorage.langdate = 2;
+    }
+    langdate();
+}
