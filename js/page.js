d01c65da09f2f3a2bb7827383c94a223af2391e5
Quinn0823 <3547347506@qq.com>
2022-11-19 13:40:21 +0800

    ⚙ Provided ''Forever'', ''This Visit Only'' and ''This Page Only'' 3 language setting options. 🔛 Even if you close your browser or computer, your language settings can be valid on all pages. 💻 Introduces new JavaScript code while keeping the old style, faster access and more stable language settings.
    Former-commit-id: a05463b9c248e6df61fa68dec0a245fb915c9bcb
    Former-commit-id: b365f0bb69100631b90575d35af648ed7b663028
    Former-commit-id: 1bc787768886081a2d7540286353cfbfafc5a586

--- js/page.js
+++ js/page.js
@@ -1,10 +1,9 @@
 var path = window.location.pathname, title_en = document.title, title_cns = document.title;
+
 if(path == "/index.html" || path == "/") {
     title_cns = "首页";
 } else if(path == "/projects.html") {
     title_cns = "项目";
-} else if(path == "/media.html") {
-    title_cns = "媒体";
 } else if(path == "/media/music.html") {
     title_cns = "音乐";
 } else if(path == "/media/photos.html") {
@@ -17,29 +16,50 @@ if(path == "/index.html" || path == "/") {
     title_cns = "关于";
 }
 
-var news_en = "<ul><li>Even if you close your browser or computer, your language settings will always be valid on all pages. </li><li>Introduces new code while keeping the old style, faster access and more stable language settings. </li>";
-var news_cns = "<ul><li>即使您关闭浏览器或计算机，您的语言设置在所有页面上也将始终有效。</li><li>在保持旧风格的同时引入新代码，访问速度更快，语言设置更稳定。</li>";
 
-var next_en = "<ul><li>Content about me will be written. </li><li>Provides ''Forever'', ''This Visit Only'' and ''This Page Only'' 3 language setting options. </li><li>You can clear the language setting data. </li></ul>";
-var next_cns = "<ul><li>关于我的内容将被写入。</li><li>提供「永久」「仅本次访问」和「仅本页面」三个语言设置选项。</li><li>可以清除语言设置数据。</li></ul>";
+var news_en = "<ul><li><sup class='new'>NEW</sup>Provided ''Forever'', ''This Visit Only'' and ''This Page Only'' 3 language setting options. </li><li>Even if you close your browser or computer, your language settings can be valid on all pages. </li><li>Introduces new code while keeping the old style, faster access and more stable language settings. </li>";
+var news_cns = "<ul><li><sup class='new'>新增</sup>提供「永久」「仅本次访问」和「仅本页面」三个语言设置选项。</li><li>即使您关闭浏览器或计算机，您的语言设置在所有页面上也可以有效。</li><li>在保持旧风格的同时引入新代码，访问速度更快，语言设置更稳定。</li>";
 
-var n_version = "12.1.2";
+var next_en = "<ul><li>Content about me will be written. </li><li>You can clear the language setting data. </li></ul>";
+var next_cns = "<ul><li>关于我的内容将被写入。</li><li>可以清除语言设置数据。</li></ul>";
 
-var date_en = "Aug. 3, 2022";
-var date_cns = "2022年8月3日";
+var n_version = "Quinn-Web 12.3";
 
-var progress = "<progress value='10' max='100'></progress>";
+var date_en = "Nov. 20, 2022";
+var date_cns = "2022年11月20日";
 
-var header_en = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a href='/media.html'>Media</a><ul><li><a href='/media/music.html'>Music</a></li><li><a href='/media/photos.html'>Photos</a></li><li><a href='/media/videos.html'>Videos</a></li></ul></li><li><a href='/lab.html'>lab</a></li><li class='about'><a href='/about.html'>About</a></li></ul></div><h1>" + title_en + "</h1><hr />";
-var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>首页</a></li><li><a href='/projects.html'>项目</a></li><li><a href='/media.html'>媒体</a><ul><li><a href='/media/music.html'>音乐</a></li><li><a href='/media/photos.html'>照片</a></li><li><a href='/media/videos.html'>视频</a></li></ul></li><li><a href='/lab.html'>实验室</a></li><li class='about'><a href='/about.html'>关于</a></li></ul></div><h1>" + title_cns + "</h1><hr />";
+var progress = 60;
 
-var footer_en = "<span>The next version: " + n_version + "</span><details><summary>✨ What has been updated now? </summary><hr /><span>" + news_en + "</span></details><details><summary>⏩ What else will be updated in the next version? " + progress+ "</summary><hr /><span>" + next_en + "</span></details><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 Last Update: " + date_en + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved.</span>";
-var footer_cns = "<span>下一个版本：" + n_version + "</span><details><summary>✨ 现已更新</summary><hr /><span>" + news_cns + "</span></details><details><summary>⏩ 下一个版本还将更新 " + progress+ "</summary><hr /><span>" + next_cns + "</span></details><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 最近更新：" + date_cns + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " 邱琦轩 保留所有权利。</span>";
+var header_en = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a>Media</a><ul><li><a href='/media/music.html'>Music</a></li><li><a href='/media/photos.html'>Photos</a></li><li><a href='/media/videos.html'>Videos</a></li></ul></li><li><a href='/lab.html'>Lab</a></li><li class='about'><a href='/about.html'>About</a></li></ul></div><h1>" + title_en + "</h1><hr />";
+var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>首页</a></li><li><a href='/projects.html'>项目</a></li><li><a>媒体</a><ul><li><a href='/media/music.html'>音乐</a></li><li><a href='/media/photos.html'>照片</a></li><li><a href='/media/videos.html'>视频</a></li></ul></li><li><a href='/lab.html'>实验室</a></li><li class='about'><a href='/about.html'>关于</a></li></ul></div><h1>" + title_cns + "</h1><hr />";
+
+var langset_en = "<input type='radio' name='lset' id='for' onclick='langset(this.id)'>Forever<input type='radio' name='lset' id='vis' onclick='langset(this.id)'>This Visit Only<input type='radio' name='lset' id='pag' onclick='langset(this.id)'>This Page Only";
+var langset_cns = "<input type='radio' name='lset' id='for' onclick='langset(this.id)'>永远<input type='radio' name='lset' id='vis' onclick='langset(this.id)'>仅本次访问<input type='radio' name='lset' id='pag' onclick='langset(this.id)'>仅本页面";
+
+var footer_en = "<hr><div id='footeri'><span>The next version: " + n_version + "</span> <progress value='" + progress+ "' max='100'></progress><details><summary>✨ What has been updated now? </summary><hr /><span>" + news_en + "</span></details><details><summary>⏩ What else will be updated in the next version?</summary><hr /><span>" + next_en + "</span></details>Language Setting (Except United States - English): " + langset_en + "<div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 Last Update: " + date_en + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved.</span></div>";
+var footer_cns = "<hr><div id='footeri'><span>下一个版本：" + n_version + "</span> <progress value='" + progress+ "' max='100'></progress><details><summary>✨ 现已更新</summary><hr /><span>" + news_cns + "</span></details><details><summary>⏩ 下一个版本还将更新</summary><hr /><span>" + next_cns + "</span></details>语言设置（除 United States - English 外）：" + langset_cns + "<div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 最近更新：" + date_cns + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " 邱琦轩 保留所有权利。</span><div>";
+
+var ldate = 1;
+
+
+function langset(set) {
+    if(set == "for") {
+        localStorage.lset = 1;
+        sessionStorage.langdate = "";
+    } else if(set == "vis") {
+        localStorage.lset = 2;
+        localStorage.langdate = "";
+    } else if(set == "pag") {
+        localStorage.lset = 3;
+        localStorage.langdate = "";
+        sessionStorage.langdate = "";
+    }
+}
 
 langdate();
 
 function langdate() {
-    if(localStorage.langdate == 2) { //Chinese
+    if(localStorage.langdate == 2 || sessionStorage.langdate == 2 || ldate == 2) { //Chinese
         document.title = title_cns + " | Quinn-Web";
 
         document.getElementById("header").innerHTML = header_cns;
@@ -47,6 +67,16 @@ function langdate() {
 
         document.getElementById("en").style.display="none";
         document.getElementById("cns").style.display="inline";
+
+        if(localStorage.lset == 1) {
+            document.getElementById("for").checked = true;
+        } else if(localStorage.lset == 2) {
+            document.getElementById("vis").checked = true;
+        } else if(localStorage.lset == 3) {
+            document.getElementById("pag").checked = true; 
+        } else {
+            document.getElementById("for").checked = true;
+        }
     } else { //English
         document.title = title_en + " | Quinn-Web";
 
@@ -55,14 +85,36 @@ function langdate() {
 
         document.getElementById("en").style.display="inline";
         document.getElementById("cns").style.display="none";
+
+        if(localStorage.lset == 1) {
+            document.getElementById("for").checked = true;
+        } else if(localStorage.lset == 2) {
+            document.getElementById("vis").checked = true; 
+        } else if(localStorage.lset == 3) {
+            document.getElementById("pag").checked = true;
+        } else {
+            document.getElementById("for").checked = true;
+
+        }
     }
 } 
 
+
 function langchange() {
     if(document.getElementById("lang").value=="1") {
-        localStorage.langdate = 1;
+        if(localStorage.lset == 1) {
+            localStorage.langdate = 1;
+        } else if(localStorage.lset == 2) {
+            sessionStorage.langdate = 1;
+        }
+        ldate = 1;
     } else if(document.getElementById("lang").value == "2") {
-        localStorage.langdate = 2;
+        if(localStorage.lset == 1) {
+            localStorage.langdate = 2;
+        } else if(localStorage.lset == 2) {
+            sessionStorage.langdate = 2;
+        }
+        ldate = 2;
     }
     langdate();
 }
