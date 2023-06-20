e8ce7d369071beb728af5dc6f89ea0337d7b5cae
Quinn Qiu <3547347506@qq.com>
2023-06-20 17:56:27 +0800

    ⬆️ Version information update
    Former-commit-id: c3e0691ca616cf0f1e9da3ff19132ed0db7d8130
    Former-commit-id: a18846620c6a8412997ec18231a6a77cf3051143
    Former-commit-id: ff5f8168a968725e0c86276d6973cf370b3a40cb
    Former-commit-id: 2468e076c7efd85067494fa7ca109854d409459b
    Former-commit-id: bd7703a7bf01b120d354b7b55da61f9ce6b7496d
    Former-commit-id: 5bfb9df303a658e1091f4f8dc76076e2c626a46a
    Former-commit-id: 2b0e9a8116637652857b76cb082f397e4a478123
    Former-commit-id: eeb0a3e654140f47819921989d191203e2ed27d0
    Former-commit-id: 1eceb823d293465c7d34c4842f5a4dbca60dc08c
    Former-commit-id: d0d0c53ae58f1e06290687e5a472ae4a91d89115
    Former-commit-id: a253b59999ac9d6897312c095b506d6406e6f36e
    Former-commit-id: a984c36250c406e0a9e2afc5b95dc7d68a6d7781
    Former-commit-id: 80f1d9c67cd2669c0b8a97c64efff8df4ff217d4

--- js/page.js
+++ js/page.js
@@ -17,18 +17,28 @@ if (path == "/index.html" || path == "/") {
     title_cns = "关于";
 }
 
-/*
-<details><summary>Title</summary><hr /><ul><li>First</li><li>Secend</li><li>Third</li></ul></details>
-*/
-var news_en = "<span>Quinn-Web 12.2.2<sup class='nt'>NEXT</sup></span><details><summary>⏩ What will update next?</summary><hr /><ul><li>🈴 All Chinese content will be translated.</li></ul></details>" + "<hr /><span>Quinn-Web 12.2.1<sup class='nw'>NOW</sup></span><details><summary>✨ What HAVE BEEN UPDATED now? </summary><hr /><ul><li>🏹 Brand new TIMELINE function.</li><li>👦🏻 New self-introduction.</li></ul></details><details><summary>🥚 Stinger</summary><hr /><ul><li>🕯 Age is calculated by JavaScript.</li></ul></details>";
-var news_cns = "<span>Quinn-Web 12.2.2<sup class='nt'>下一</sup></span><details><summary>⏩ 即将更新</summary><hr /><ul><li>🈴 所有中文内容将被翻译.</li></ul></details>" + "<hr /><span>Quinn-Web 12.2.1<sup class='nw'>当前</sup></span><details><summary>✨ 现已更新</summary><hr /><ul><li>🏹 新时间线功能.</li><li>👦🏻 新自我介绍.</li></ul></details><details><summary>🥚 彩蛋</summary><hr /><ul><li>🕯 年龄由 JavaScript 计算.</li></ul></details>";
-
-var date_en = "Jun. 20, 2023";
-var date_cns = "2023年6月20日";
-
 var header_en = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>Home</a></li><li><a href='/projects.html'>Projects</a></li><li><a>Media</a><ul><li><a href='/media/music.html'>Music</a></li><li><a href='/media/photos.html'>Photos</a></li><li><a href='/media/videos.html'>Videos</a></li></ul></li><li><a href='/lab.html'>Lab</a></li><li class='about'><a href='/about.html'>About</a></li></ul></div><h1>" + title_en + "</h1><hr>";
 var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/index.html'>首页</a></li><li><a href='/projects.html'>项目</a></li><li><a>媒体</a><ul><li><a href='/media/music.html'>音乐</a></li><li><a href='/media/photos.html'>照片</a></li><li><a href='/media/videos.html'>视频</a></li></ul></li><li><a href='/lab.html'>实验室</a></li><li class='about'><a href='/about.html'>关于</a></li></ul></div><h1>" + title_cns + "</h1><hr>";
 
+
+/*
+<details><summary>Title</summary><hr /><ul><li>First</li><li>Secend</li><li>Third</li></ul></details>
+*/
+var now_ver = "12.2.2";
+var next_ver = "None";
+
+var next_cont_en = "<li>None</li>";
+var next_cont_cns = "<li>None</li>";
+
+var now_cont_en = "<li>🈴 All Chinese contents have been translated.</li>";
+var now_cont_cns = "<li>🈴 所有中文内容已被翻译.</li>";
+
+var news_en = "<span>Quinn-Web " + next_ver + "<sup class='nt'>NEXT</sup></span><details><summary>⏩ What will be updated?</summary><hr /><ul>" +next_cont_en + "</ul></details><hr /><span>Quinn-Web " + now_ver + "<sup class='nw'>NOW</sup></span><details><summary>✨ What have been update? </summary><hr /><ul>" + now_cont_en + "</ul></details>";
+var news_cns = "<span>Quinn-Web " + next_ver + "<sup class='nt'>下一</sup></span><details><summary>⏩ 即将更新</summary><hr /><ul>" +next_cont_cns + "</ul></details><hr /><span>Quinn-Web " + now_ver + "<sup class='nw'>当前</sup></span><details><summary>✨ 现已更新</summary><hr /><ul></ul></details>";
+
+
+var date_en = "Jun. 20, 2023";
+var date_cns = "2023年6月20日";
 
 var footer_en = "<hr><div id='footeri'>" + news_en + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '>📅 Last Update: " + date_en + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. | <a href='https://github.com/Quinn0823/quinn0823.github.io'  target='_blank'><i class='fa-brands fa-github'></i></a></span></div>";
 var footer_cns = "<hr><div id='footeri'>" + news_cns + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '>📅 最近更新：" + date_cns + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利。 | <a href='https://github.com/Quinn0823/quinn0823.github.io'  target='_blank'><i class='fa-brands fa-github'></i></a></span></div>";
