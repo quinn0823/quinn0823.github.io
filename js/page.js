47556a30c3c0797b7eab30a9dcf4a60ca6b40887
Quinn Qiu <quinnqiu0823@icloud.com>
2023-08-18 18:24:20 +0800

    📝 fonts update
    Former-commit-id: 24f97a67ebd15fe55a86935f5e1dd3516115ce20
    Former-commit-id: 24fabe880382fd19e06cd9329a500ee730bdc0fa
    Former-commit-id: 69c86a81dc41f42c99af3f4953324491cdd982d1
    Former-commit-id: fe0e6df767f7b9d6034273ea82763e2bfd4fbea6
    Former-commit-id: be85448fc3db969e80c9fcdd9c062653d65c9833
    Former-commit-id: ec8284259eda83b35350476d355c77ffee31026b
    Former-commit-id: aa9b92ab087574d669c54da16d2762007ff9b7fd
    Former-commit-id: dc8f2cb2d9694ebe8fe1fc3f15ff3320755b136e
    Former-commit-id: ccea6b5f0ea90b56250ff9603100701fc65fcd3e
    Former-commit-id: f73a71b4137ad0b407d42092b6955d3ffaa67c92
    Former-commit-id: 640c8d5482ab8f38290277e14d2c8ba4322c3619
    Former-commit-id: 0c49b11b374dc120033fd77f61e12722227fd262
    Former-commit-id: 251097320f4c80cf8851638c0e1b2605f2e71a7d
    Former-commit-id: 2650d4785870faffb50fb4f34db7ae1d2fb21235

--- js/page.js
+++ js/page.js
@@ -20,8 +20,8 @@ if (title_en == "Home") {
     title_cns = "此网站";
 } else if (title_en == "Textbook Download Helper") {
     title_cns = "教材下载助手";
-} else if (title_en == "Digital Dot Matrix") {
-    title_cns = "数字点阵";
+} else if (title_en == "Digital Dot Matrix Tool") {
+    title_cns = "数字点阵工具";
 } 
 var header_en = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects/index.html'>Projects</a></li><li><a href='/qs/index.html'>QS</a></li><li><a href='/music.html'>Music</a></li><li><a href='/lab.html'>Lab</a></li><li class='about'><a href='/website.html'>Website</a></li></ul></div><h1>" + title_en + "</h1><hr>";
 var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>首页</a></li><li><a href='/projects/index.html'>项目</a></li><li><a href='/qs/index.html'>标准</a></li><li><a href='/music.html'>音乐</a></li><li><a href='/lab.html'>实验室</a></li><li class='about'><a href='/website.html'>此网站</a></li></ul></div><h1>" + title_cns + "</h1><hr>";
@@ -40,15 +40,15 @@ var next_cont_cns = "<p>所有符号将被重构;</p><p>新的圆角字体</p><p
 var now_cont_en = "<p>Welcome! </p>";
 var now_cont_cns = "<p>欢迎! </p>";
 
-var news_en = "<span> " + next_ver + " - NEXT</span><div class='cont'><details><summary><img src='/images/svgs/tray.and.arrow.down.fill.svg' /> What will be updated?</summary><hr /><div class='cont'>" +next_cont_en + "</div></details></div><hr /><span>" + now_ver + "  - NOW</span><div class='cont'><details open><summary><img src='/images/svgs/newspaper.fill.svg' /> What have been update? </summary><hr /><div class='cont'>" + now_cont_en + "</div></details></div>";
-var news_cns = "<span> " + next_ver + " - 下一</span><div class='cont'><details><summary><img src='/images/svgs/tray.and.arrow.down.fill.svg' /> 即将更新</summary><hr /><div class='cont'>" +next_cont_cns + "</div></details></div><hr /><span>" + now_ver + " - 当前</span><div class='cont'><details open><summary><img src='/images/svgs/newspaper.fill.svg' /> 现已更新</summary><hr /><div class='cont'>" + now_cont_cns + "</div></details></div>";
+var news_en = "<span> " + next_ver + " - NEXT</span><div class='cont'><details><summary><img src='/images/svgs/sparkles.rectangle.stack.svg' /> What will be updated?</summary><hr /><div class='cont'>" +next_cont_en + "</div></details></div><hr /><span>" + now_ver + "  - NOW</span><div class='cont'><details open><summary><img src='/images/svgs/checkmark.rectangle.stack.fill.svg' /> What have been update? </summary><hr /><div class='cont'>" + now_cont_en + "</div></details></div>";
+var news_cns = "<span> " + next_ver + " - 下一</span><div class='cont'><details><summary><img src='/images/svgs/sparkles.rectangle.stack.svg' /> 即将更新</summary><hr /><div class='cont'>" +next_cont_cns + "</div></details></div><hr /><span>" + now_ver + " - 当前</span><div class='cont'><details open><summary><img src='/images/svgs/checkmark.rectangle.stack.fill.svg' /> 现已更新</summary><hr /><div class='cont'>" + now_cont_cns + "</div></details></div>";
 
 
-var date_en = "Jul 26, 2023";
-var date_cns = "2023年7月26日";
+var date_en = "Aug. 18, 2023";
+var date_cns = "2023年8月18日";
 
-var footer_en = "<hr><div id='footeri'>" + news_en + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><i class='fa-solid fa-calendar-days'></i> Last Update: " + date_en + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. </span></div>";
-var footer_cns = "<hr><div id='footeri'>" + news_cns + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><i class='fa-solid fa-calendar-days'></i> 最近更新：" + date_cns + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利. </span></div>";
+var footer_en = "<hr><div id='footeri'>" + news_en + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><img src='/images/svgs/calendar.badge.clock.svg' /> Last Update: " + date_en + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. </span></div>";
+var footer_cns = "<hr><div id='footeri'>" + news_cns + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><img src='/images/svgs/calendar.badge.clock.svg' /> 最近更新：" + date_cns + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利. </span></div>";
 
 var ldate = 1;
 
