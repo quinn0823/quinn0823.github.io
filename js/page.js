b6caf65ecff89118334d9a68e23ebe20243c475e
Quinn0823 <quinnqiu0823@icloud.com>
2023-09-05 16:11:49 +0800

    🔠 lang code update
    Former-commit-id: 59d2d8906a39893ad1332a4b234341f90b115c1d
    Former-commit-id: f7a6a33ba3c5b5d84c6f48a9881824ab711f8b50
    Former-commit-id: 4c258c72d6505622d48fb8224a59bae73bd03d68
    Former-commit-id: 5dd7a6620c14b8bec87df1df55b910c6d7c86a04
    Former-commit-id: 51884935550c747e98c162b70311bf5e1f386828
    Former-commit-id: 8b23328a777025f6c573f07631293cfc4cd7af3c
    Former-commit-id: 8f0f57efa44cc1aedd31509f07df7d94245fc5c2
    Former-commit-id: 9696e61b303d780bc13c54c06066ea2c405f215d
    Former-commit-id: 53c9f593d66a009f8f76f3215c34385f66c94be5
    Former-commit-id: 434b0e08f6f3effa5a830b02439f94dda8538fe7
    Former-commit-id: 317c2257057129890e3d49f3998697b87e59e018
    Former-commit-id: 9aa9d12af428f56ea8326e4b8437839c8e8578d8
    Former-commit-id: 6308d428b20d27c37a948c51a724e14f456b1c41
    Former-commit-id: d296f4558f16e58e3dcec0b926ff35ccf44ca0fc

--- js/page.js
+++ js/page.js
@@ -1,32 +1,32 @@
 var path = window.location.pathname
-var title_en = document.title
-var title_cns = document.title;
+var title_en_us = document.title
+var title_zh_cn = document.title;
 
-if (title_en == "Home") {
-    title_cns = "首页";
-} else if (title_en == "Projects") {
-    title_cns = "项目";
-} else if (title_en == "App Library") {
-    title_cns = "App 资源库"
-} else if (title_en == "QuinnStandard") {
-    title_cns = "Quinn 标准";
-}  else if (title_en == "Music") {
-    title_cns = "音乐";
-} else if (title_en == "Lab") {
-    title_cns = "实验室";
-} else if (title_en == "Website") {
-    title_cns = "此网站";
-} else if (title_en == "Textbook Download Helper") {
-    title_cns = "教材下载助手";
-} else if (title_en == "Digital Dot Matrix Tool") {
-    title_cns = "数字点阵工具";
-} else if (title_en == "QS2023 01-E3") {
+if (title_en_us == "Home") {
+    title_zh_cn = "首页";
+} else if (title_en_us == "Projects") {
+    title_zh_cn = "项目";
+} else if (title_en_us == "App Library") {
+    title_zh_cn = "App 资源库"
+} else if (title_en_us == "QuinnStandard") {
+    title_zh_cn = "Quinn 标准";
+}  else if (title_en_us == "Music") {
+    title_zh_cn = "音乐";
+} else if (title_en_us == "Lab") {
+    title_zh_cn = "实验室";
+} else if (title_en_us == "Website") {
+    title_zh_cn = "此网站";
+} else if (title_en_us == "Textbook Download Helper") {
+    title_zh_cn = "教材下载助手";
+} else if (title_en_us == "Digital Dot Matrix Tool") {
+    title_zh_cn = "数字点阵工具";
+} else if (title_en_us == "QS2023 01-E3") {
     
-} else if (title_en == "QS2023 02") {
+} else if (title_en_us == "QS2023 02") {
     
 }
-var header_en = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects/index.html'>Projects</a></li><li><a href='/qs/index.html'>QS</a></li><li><a href='/music.html'>Music</a></li><li class='about'><a href='/website.html'>Website</a></li><li class='about'><a href='/markdown/'>Markdown</a></li></ul></div><h1>" + title_en + "</h1><hr>";
-var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>首页</a></li><li><a href='/projects/index.html'>项目</a></li><li><a href='/qs/index.html'>标准</a></li><li><a href='/music.html'>音乐</a></li><li class='about'><a href='/website.html'>此网站</a></li><li class='about'><a href='/markdown/'>Markdown</a></li></ul></div><h1>" + title_cns + "</h1><hr>";
+var header_en_us = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>Home</a></li><li><a href='/projects/index.html'>Projects</a></li><li><a href='/qs/index.html'>QS</a></li><li><a href='/music.html'>Music</a></li><li class='about'><a href='/website.html'>Website</a></li><li class='about'><a href='/markdown/'>Markdown</a></li></ul></div><h1>" + title_en_us + "</h1><hr>";
+var header_zh_cn = "<div class='list'><ul><img src='/images/logo.png'/><li><a href='/'>首页</a></li><li><a href='/projects/index.html'>项目</a></li><li><a href='/qs/index.html'>标准</a></li><li><a href='/music.html'>音乐</a></li><li class='about'><a href='/website.html'>此网站</a></li><li class='about'><a href='/markdown/'>Markdown</a></li></ul></div><h1>" + title_zh_cn + "</h1><hr>";
 
 
 
@@ -36,21 +36,21 @@ var header_cns = "<div class='list'><ul><img src='/images/logo.png'/><li><a href
 var now_ver = "Quinn-Web 12.4";
 var next_ver = "Quinn-Web 12.5";
 
-var next_cont_en = "<p>All symbols will be refactored;</p><p>New round font.</p><p>...</p>";
-var next_cont_cns = "<p>所有符号将被重构;</p><p>新的圆角字体</p><p>...</p>";
+var next_cont_en_us = "<p>All symbols will be refactored;</p><p>New round font.</p><p>...</p>";
+var next_cont_zh_cn = "<p>所有符号将被重构;</p><p>新的圆角字体</p><p>...</p>";
 
-var now_cont_en = "<p>Welcome! </p>";
-var now_cont_cns = "<p>欢迎! </p>";
+var now_cont_en_us = "<p>Welcome! </p>";
+var now_cont_zh_cn = "<p>欢迎! </p>";
 
-var news_en = "<span> " + next_ver + " - NEXT</span><div class='cont'><details><summary><img src='/images/svgs/primary/sparkles.rectangle.stack.svg' /> What will be updated?</summary><hr /><div class='cont'>" +next_cont_en + "</div></details></div><hr /><span>" + now_ver + "  - NOW</span><div class='cont'><details open><summary><img src='/images/svgs/primary/checkmark.rectangle.stack.fill.svg' /> What have been update? </summary><hr /><div class='cont'>" + now_cont_en + "</div></details></div>";
-var news_cns = "<span> " + next_ver + " - 下一</span><div class='cont'><details><summary><img src='/images/svgs/primary/sparkles.rectangle.stack.svg' /> 即将更新</summary><hr /><div class='cont'>" +next_cont_cns + "</div></details></div><hr /><span>" + now_ver + " - 当前</span><div class='cont'><details open><summary><img src='/images/svgs/primary/checkmark.rectangle.stack.fill.svg' /> 现已更新</summary><hr /><div class='cont'>" + now_cont_cns + "</div></details></div>";
+var news_en_us = "<span> " + next_ver + " - NEXT</span><div class='cont'><details><summary><img src='/images/svgs/primary/sparkles.rectangle.stack.svg' /> What will be updated?</summary><hr /><div class='cont'>" +next_cont_en_us + "</div></details></div><hr /><span>" + now_ver + "  - NOW</span><div class='cont'><details open><summary><img src='/images/svgs/primary/checkmark.rectangle.stack.fill.svg' /> What have been update? </summary><hr /><div class='cont'>" + now_cont_en_us + "</div></details></div>";
+var news_zh_cn = "<span> " + next_ver + " - 下一</span><div class='cont'><details><summary><img src='/images/svgs/primary/sparkles.rectangle.stack.svg' /> 即将更新</summary><hr /><div class='cont'>" +next_cont_zh_cn + "</div></details></div><hr /><span>" + now_ver + " - 当前</span><div class='cont'><details open><summary><img src='/images/svgs/primary/checkmark.rectangle.stack.fill.svg' /> 现已更新</summary><hr /><div class='cont'>" + now_cont_zh_cn + "</div></details></div>";
 
 
-var date_en = "Aug. 18, 2023";
-var date_cns = "2023年8月18日";
+var date_en_us = "Aug. 18, 2023";
+var date_zh_cn = "2023年8月18日";
 
-var footer_en = "<hr><div id='footeri'>" + news_en + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><img src='/images/svgs/primary/calendar.badge.clock.svg' /> Last Update: " + date_en + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. </span></div>";
-var footer_cns = "<hr><div id='footeri'>" + news_cns + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><img src='/images/svgs/primary/calendar.badge.clock.svg' /> 最近更新：" + date_cns + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利. </span></div>";
+var footer_en_us = "<hr><div id='footeri'>" + news_en_us + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><img src='/images/svgs/primary/calendar.badge.clock.svg' /> Last Update: " + date_en_us + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. </span></div>";
+var footer_zh_cn = "<hr><div id='footeri'>" + news_zh_cn + "<hr /><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文 (中国大陆) </option></select></div><div class='g_url'><span style='text-align: center; display:block; '><img src='/images/svgs/primary/calendar.badge.clock.svg' /> 最近更新：" + date_zh_cn + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利. </span></div>";
 
 var ldate = 1;
 
@@ -71,21 +71,21 @@ function langset(set) {
 
 function langdate() {
     if (localStorage.langdate == 2) { //Chinese
-        document.title = title_cns + " | Quinn-Web";
+        document.title = title_zh_cn + " | Quinn-Web";
 
-        document.getElementById("header").innerHTML = header_cns;
-        document.getElementById("footer").innerHTML = footer_cns;
+        document.getElementById("header").innerHTML = header_zh_cn;
+        document.getElementById("footer").innerHTML = footer_zh_cn;
 
-        document.getElementById("en").style.display = "none";
-        document.getElementById("cns").style.display = "inline";
+        document.getElementById("en-us").style.display = "none";
+        document.getElementById("zh-cn").style.display = "inline";
     } else { //English
-        document.title = title_en + " | Quinn-Web";
+        document.title = title_en_us + " | Quinn-Web";
 
-        document.getElementById("header").innerHTML = header_en;
-        document.getElementById("footer").innerHTML = footer_en;
+        document.getElementById("header").innerHTML = header_en_us;
+        document.getElementById("footer").innerHTML = footer_en_us;
 
-        document.getElementById("en").style.display = "inline";
-        document.getElementById("cns").style.display = "none";
+        document.getElementById("en-us").style.display = "inline";
+        document.getElementById("zh-cn").style.display = "none";
     }
 }
 
