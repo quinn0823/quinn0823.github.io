104081e7113b59ea8afae6e9dc87736c34b906fa
Quinn0823 <quinnqiu0823@icloud.com>
2023-09-17 18:51:02 +0800

    🔠 lang code update
    Former-commit-id: c3dbc7c508e00a8aced325a0c50d0e65b3c80b23
    Former-commit-id: f65745691a22254569df552126671136f5e953fc
    Former-commit-id: 0d8ecfad814d541d1ee28887a160cac6aba5c77e
    Former-commit-id: 41b438f02f0b4390003bbf63f24025f52c68ab63
    Former-commit-id: d06727336f3a35935a0b7ca1416ad91d9375fc25
    Former-commit-id: e197cd9949eb2223b003f15a10b690d68293db05
    Former-commit-id: ef670cffb5ed47370b1eb5cc413902a17362c5b7
    Former-commit-id: 3a88d61f3a0e47550c29cc4ee0bc7315723853df
    Former-commit-id: a910338064bd55793be5bccf66ba3388a5c8266a
    Former-commit-id: 6366def499537207f818d5a976b273acdf9da68a
    Former-commit-id: 7faded1f64507a027b7570f77d1079450505b83a
    Former-commit-id: 911b2ae296c84f602bc6c04aaa55eb0dbb390c2d
    Former-commit-id: 9f7c1ea46b51b500a110a206cdfcbee9b2511ec1
    Former-commit-id: c2969c2a32362f74ff2dbcd6e18583bc6f9fb33b

--- js/page.js
+++ js/page.js
@@ -1,29 +1,29 @@
 /* --- 网页标题 --- */
 
-var title_en_us = document.title
-var title_zh_cn = document.title;
+var title_us = document.title
+var title_cn = document.title;
 
-if (title_en_us == "Home") {
-    title_zh_cn = "首页";
-} else if (title_en_us == "Projects") {
-    title_zh_cn = "项目";
-} else if (title_en_us == "App Library") {
-    title_zh_cn = "App 资源库"
-} else if (title_en_us == "QuinnStandard") {
-    title_zh_cn = "Quinn 标准";
-}  else if (title_en_us == "Music") {
-    title_zh_cn = "音乐";
-} else if (title_en_us == "Lab") {
-    title_zh_cn = "实验室";
-} else if (title_en_us == "Website") {
-    title_zh_cn = "此网站";
-} else if (title_en_us == "Textbook Download Helper") {
-    title_zh_cn = "教材下载助手";
-} else if (title_en_us == "Digital Dot Matrix Tool") {
-    title_zh_cn = "数字点阵工具";
-} else if (title_en_us == "QS2023 01-E3") {
+if (title_us == "Home") {
+    title_cn = "首页";
+} else if (title_us == "Projects") {
+    title_cn = "项目";
+} else if (title_us == "App Library") {
+    title_cn = "App 资源库"
+} else if (title_us == "QuinnStandard") {
+    title_cn = "Quinn 标准";
+}  else if (title_us == "Music") {
+    title_cn = "音乐";
+} else if (title_us == "Lab") {
+    title_cn = "实验室";
+} else if (title_us == "Website") {
+    title_cn = "此网站";
+} else if (title_us == "Textbook Download Helper") {
+    title_cn = "教材下载助手";
+} else if (title_us == "Digital Dot Matrix Tool") {
+    title_cn = "数字点阵工具";
+} else if (title_us == "QS2023 01-E3") {
     
-} else if (title_en_us == "QS2023 02") {
+} else if (title_us == "QS2023 02") {
     
 }
 
@@ -32,8 +32,8 @@ if (title_en_us == "Home") {
 /* --- 页眉 --- */
 
 // 菜单栏
-var header_en_us = "<div class='list'><ul><img src='images/favicon.png'/><li><a href=''>Home</a></li><li><a href='projects/'>Projects</a></li><li><a href='qs/'>QS</a></li><li><a href='music.html'>Music</a></li><li class='right'><a href='website.html'>Website</a></li></ul></div><h1>" + title_en_us + "</h1><hr>";
-var header_zh_cn = "<div class='list'><ul><img src='images/favicon.png'/><li><a href=''>首页</a></li><li><a href='projects/'>项目</a></li><li><a href='qs/'>标准</a></li><li><a href='music.html'>音乐</a></li><li class='right'><a href='website.html'>此网站</a></li></ul></div><h1>" + title_zh_cn + "</h1><hr>";
+var header_us = "<div class='list'><ul><img src='images/favicon.png'/><li><a href=''>Home</a></li><li><a href='projects/'>Projects</a></li><li><a href='qs/'>QS</a></li><li><a href='music.html'>Music</a></li><li class='right'><a href='website.html'>Website</a></li></ul></div><h1>" + title_us + "</h1><hr>";
+var header_cn = "<div class='list'><ul><img src='images/favicon.png'/><li><a href=''>首页</a></li><li><a href='projects/'>项目</a></li><li><a href='qs/'>标准</a></li><li><a href='music.html'>音乐</a></li><li class='right'><a href='website.html'>此网站</a></li></ul></div><h1>" + title_cn + "</h1><hr>";
 
 
 
@@ -49,44 +49,44 @@ var now_tag = "12.5";
 var next_tag = "12.6";
 
 // 发行内容
-var now_up_en_us = "<ul><li></li></ul>";
-var now_up_zh_cn = "<ul><li></li></ul>";
+var now_up_us = "<ul><li></li></ul>";
+var now_up_cn = "<ul><li></li></ul>";
 // 预览内容
-var next_up_en_us = "<ul><li></li></ul>";
-var next_up_zh_cn = "<ul><li></li></ul>";
+var next_up_us = "<ul><li></li></ul>";
+var next_up_cn = "<ul><li></li></ul>";
 
 // 进度
 var progress = 95;
 
 // 更新时间
 var up_year = "2023";
-var up_month_en_us = "Sep.";
-var up_month_zh_cn = "9";
+var up_month_us = "Sep.";
+var up_month_cn = "9";
 var up_day = "11"
 
-var date_en_us = up_month_en_us + " " + up_day + ", " + up_year;
-var date_zh_cn = up_year + "年" + up_month_zh_cn + "月" + up_day + "日";
+var date_us = up_month_us + " " + up_day + ", " + up_year;
+var date_cn = up_year + "年" + up_month_cn + "月" + up_day + "日";
 
 
 
 /* --- 页脚框架部分 --- */
 // 新闻框架
-var news_en_us = "<div class='lmr enlmr'><div class='left'><span>Quinn-Web " + next_tag + " - NEXT</span></div><div class='middle'><progress max='100' value='" + progress + "'></progress></div><div class='right'><span>Progress: " + progress + "%</span></div></div><details><summary>⏩ What will be updated?</summary><hr>" +next_up_en_us + "</details><hr><div class='lmr'><div class='left'><span>Quinn-Web " + now_tag + " - NOW</span></div><div class='middle'></div><div class='right'><span><a href='https://github.com/Quinn0823/quinn0823.github.io/releases/tag/" + now_tag + "' target='_blank'><i class='fa-brands fa-github'></i>Releases</a></span></div></div><details open><summary>✨ What has been update? </summary><hr>" + now_up_en_us + "</details>";
-var news_zh_cn = "<div class='lmr'><div class='left'><span>Quinn-Web " + next_tag + " - 下一版本</span></div><div class='middle'><progress max='100' value='" + progress + "'></progress></div><div class='right'><span>进展: " + progress + "%</span></div></div><details><summary>⏩ 即将更新</summary><hr>" + next_up_zh_cn + "</details><hr><div class='lmr'><div class='left'><span>Quinn-Web " + now_tag + " - 当前版本</span></div><div class='middle'></div><div class='right'><span><a href='https://github.com/Quinn0823/quinn0823.github.io/releases/tag/" + now_tag + "' target='_blank'><i class='fa-brands fa-github'></i>发行页</a></span></div></div><details open><summary>✨ 现已更新</summary><hr>" + now_up_zh_cn + "</details>";
+var news_us = "<div class='lmr enlmr'><div class='left'><span>Quinn-Web " + next_tag + " - NEXT</span></div><div class='middle'><progress max='100' value='" + progress + "'></progress></div><div class='right'><span>Progress: " + progress + "%</span></div></div><details><summary>⏩ What will be updated?</summary><hr>" +next_up_us + "</details><hr><div class='lmr'><div class='left'><span>Quinn-Web " + now_tag + " - NOW</span></div><div class='middle'></div><div class='right'><span><a href='https://github.com/Quinn0823/quinn0823.github.io/releases/tag/" + now_tag + "' target='_blank'><i class='fa-brands fa-github'></i>Releases</a></span></div></div><details open><summary>✨ What has been update? </summary><hr>" + now_up_us + "</details>";
+var news_cn = "<div class='lmr'><div class='left'><span>Quinn-Web " + next_tag + " - 下一版本</span></div><div class='middle'><progress max='100' value='" + progress + "'></progress></div><div class='right'><span>进展: " + progress + "%</span></div></div><details><summary>⏩ 即将更新</summary><hr>" + next_up_cn + "</details><hr><div class='lmr'><div class='left'><span>Quinn-Web " + now_tag + " - 当前版本</span></div><div class='middle'></div><div class='right'><span><a href='https://github.com/Quinn0823/quinn0823.github.io/releases/tag/" + now_tag + "' target='_blank'><i class='fa-brands fa-github'></i>发行页</a></span></div></div><details open><summary>✨ 现已更新</summary><hr>" + now_up_cn + "</details>";
 
 // 联系框架
 var contact = "<div class='contact'><ul><li><a href='https://github.com/Quinn0823/' target='_blank'><i class='fa-brands fa-github'></i></a></li><li><a href='mailto:quinnqiu0823@icloud.com' target='_blank'><i class='fa-solid fa-envelope'></i></a></li><li><a href='' target='_blank'><i class='fa-brands fa-x-twitter'></i></a></li><li><a href='' target='_blank'><i class='fa-brands fa-gitter'></i></a></li><li><a href='' target='_blank'><img src='/images/logos/glitch.ico' class='fa-glitch'></a></li><li class='line'></li><li><a href='' target='_blank'><i class='fa-brands fa-weixin'></i></a></li><li><a href='' target='_blank'><i class='fa-brands fa-qq'></i></a></li><li><a href='' target='_blank'><i class='fa-brands fa-weibo'></i></a></li><li><a href='' target='_blank'><i class='fa-brands fa-bilibili'></i></a></li><li><a href='' target='_blank'><i class='fa-brands fa-zhihu'></i></a></li></ul></div>";
 
 // 页脚框架
-var footer_en_us = "<hr><div id='footeri'>" + news_en_us + "<hr><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文 (中国大陆) </option></select></div><span style='text-align: center; display:block; '>📅 Last Update: " + date_en_us + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. </span></div>"/*  + contact */;
-var footer_zh_cn = "<hr><div id='footeri'>" + news_zh_cn + "<hr><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States - English</option><option value='2' selected='selected'>简体中文 (中国大陆) </option></select></div><span style='text-align: center; display:block; '>📅 最近更新：" + date_zh_cn + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利. </span></div>"/*  + contact */;
+var footer_us = "<hr><div id='footeri'>" + news_us + "<hr><div class='sel'><select id='lang' onchange='langchange()'><option value='1' selected='selected'>United States</option><option value='2'>中国大陆</option></select></div><span style='text-align: center; display:block; '>📅 Last Update: " + date_us + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu All Rights Reserved. </span></div>"/*  + contact */;
+var footer_cn = "<hr><div id='footeri'>" + news_cn + "<hr><div class='sel'><select id='lang' onchange='langchange()'><option value='1'>United States</option><option value='2' selected='selected'>中国大陆</option></select></div><span style='text-align: center; display:block; '>📅 最近更新：" + date_cn + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn Qiu 保留所有权利. </span></div>"/*  + contact */;
 
 
 /* --- 浏览器检测 --- */
 var userAgent = navigator.userAgent;
 if (userAgent.indexOf("Firefox") > -1) {
-    header_en_us = "<span class='com'>⚠️ You are using Firefox to access Quinn-Web. We highly recommend that you use <a href='https://www.google.com/chrome/' target='_blank'>Chrome</a>, <a href='https://www.microsoft.com/en-us/edge/download/' target='_blank'>Edge</a>, or <a href='https://www.apple.com/safari/' target='_blank'>Safari</a> for a better style. Although we have tried our best to adapt the CSS code of Firefox to let Quinn-Web in Firefox look similar to the others.</span>" + header_en_us;
-    header_zh_cn = "<span class='com'>⚠️ 你正在使用 Firefox 访问 Quinn-Web. 我们强烈建议你使用 <a href='https://www.google.cn/chrome/' target='_blank'>Chrome</a>, <a href='https://www.microsoft.com/zh-cn/edge/download/' target='_blank'>Edge</a>, 或者 <a href='https://www.apple.com.cn/safari/' target='_blank'>Safari 浏览器</a> 以获得更好的样式. 尽管我们已经尽了最大的努力让 Quinn-Web 在 Firefox 中看起来和其他的相似. (URLs 已针对中国大陆地区做出适应性调整)</span>" + header_zh_cn;
+    header_us = "<span class='com'>⚠️ You are using Firefox to access Quinn-Web. We highly recommend that you use <a href='https://www.google.com/chrome/' target='_blank'>Chrome</a>, <a href='https://www.microsoft.com/us/edge/download/' target='_blank'>Edge</a>, or <a href='https://www.apple.com/safari/' target='_blank'>Safari</a> for a better style. Although we have tried our best to adapt the CSS code of Firefox to let Quinn-Web in Firefox look similar to the others.</span>" + header_us;
+    header_cn = "<span class='com'>⚠️ 你正在使用 Firefox 访问 Quinn-Web. 我们强烈建议你使用 <a href='https://www.google.cn/chrome/' target='_blank'>Chrome</a>, <a href='https://www.microsoft.com/cn/edge/download/' target='_blank'>Edge</a>, 或者 <a href='https://www.apple.com.cn/safari/' target='_blank'>Safari 浏览器</a> 以获得更好的样式. 尽管我们已经尽了最大的努力让 Quinn-Web 在 Firefox 中看起来和其他的相似. (URLs 已针对中国大陆地区做出适应性调整)</span>" + header_cn;
 } 
 
 
@@ -94,21 +94,21 @@ if (userAgent.indexOf("Firefox") > -1) {
 // 显示语言
 function langdate() {
     if (localStorage.langdate == 2) { //Chinese
-        document.title = title_zh_cn + " | Quinn-Web";
+        document.title = title_cn + " | Quinn-Web";
 
-        document.getElementById("header").innerHTML = header_zh_cn;
-        document.getElementById("footer").innerHTML = footer_zh_cn;
+        document.getElementById("header").innerHTML = header_cn;
+        document.getElementById("footer").innerHTML = footer_cn;
 
-        document.getElementById("en-us").style.display = "none";
-        document.getElementById("zh-cn").style.display = "inline";
+        document.getElementById("us").style.display = "none";
+        document.getElementById("cn").style.display = "inline";
     } else { //English
-        document.title = title_en_us + " | Quinn-Web";
+        document.title = title_us + " | Quinn-Web";
 
-        document.getElementById("header").innerHTML = header_en_us;
-        document.getElementById("footer").innerHTML = footer_en_us;
+        document.getElementById("header").innerHTML = header_us;
+        document.getElementById("footer").innerHTML = footer_us;
 
-        document.getElementById("en-us").style.display = "inline";
-        document.getElementById("zh-cn").style.display = "none";
+        document.getElementById("us").style.display = "inline";
+        document.getElementById("cn").style.display = "none";
     }
 }
 
