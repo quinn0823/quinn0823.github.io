11064df03a9e85b9003489b8bcdb7c4156b94178
Quinn0823 <quinnqiu0823@icloud.com>
2024-05-14 21:23:37 +0800

    ➕ new copy.js
    Former-commit-id: f5820b560e21c34dd01654773ef097145831fcc5
    Former-commit-id: 82556bfa7e3e349c1e1a81626ef878c2c6baee58
    Former-commit-id: f29e41703b501336bd1a109c6473a897388638ce

--- js/copy.js
+++ js/copy.js
@@ -1,26 +1,16 @@
-document.write("<textarea id='text_box'></textarea>");
+var clipboard = new ClipboardJS('.copy_text');
 
-function copyInput(inputText) { 
-    var text = document.getElementById("text_box");
-    text.value = inputText;
-    text.select();//选中文本
-    document.execCommand("copy");
-    copyAlert(inputText);
-}
+clipboard.on('success', function(e) {
+    alert("\"" + e.text + "\" has been copied.");
 
-function copyElement() {
-    // var text = document.getElementById("text").innerHTML;
-    // var input = document.getElementById("input");
-    // input.value = text;
-    // input.select();//选中文本
-    // document.execCommand("copy");
-    // alert("Copied!");
-}
+    // console.info('Action:', e.action);
+    // console.info('Text:', e.text);
+    // console.info('Trigger:', e.trigger);
 
-function copyAlert(textCopy) {
-    if(localStorage.langdate == 2) {
-        alert("\"" + textCopy + "\" 已被复制!");
-    } else {
-        alert("\"" + textCopy + "\" has been copied!");
-    }
-}
+    // e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    // console.error('Action:', e.action);
+    // console.error('Trigger:', e.trigger);
+});
