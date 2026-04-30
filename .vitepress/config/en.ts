c10d9c174a90ca4f1ae613893bf1f1fb1b17f2e4
Jonathan Chiu <quinnqiu0823@icloud.com>
2026-04-30 12:52:53 +0800

    Fix the English translation of proverbs—"尚", "still"

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -20,7 +20,7 @@ export const en = defineConfig({
     },
 
     footer: {
-      message: 'With principles but no techniques, techniques can be learned; with techniques but no principles, one goes no further than technique.<br />Licensed under a <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0 license</a>.',
+      message: 'With principles but no techniques, techniques can still be learned; with techniques but no principles, one goes no further than technique.<br />Licensed under a <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0 license</a>.',
       copyright: `Copyright © ${copyrightYear} <a href="https://github.com/quinn0823" target="_blank">Jonathan Chiu</a>`
     },
 
